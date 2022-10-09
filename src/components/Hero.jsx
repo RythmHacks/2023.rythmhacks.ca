6ed8b20d86fc6ed6957d59cfdbfb68284a34b12f
Harryyoshi <75763081+HarryWang05@users.noreply.github.com>
2022-10-08 20:17:35 -0400

    Add more content to navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@
 const Hero = () => {
     return (
         <div id='hero'>
-            <br/>
             <p>Interested in CS and tech? Want to prizes while developing your coding skills?</p>
             <p><b>Join us @ [DATE AND TIME]</b></p>
             <button>Register Now</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className='nav'>
-        <a href='/' className='nav-logo'>Site Name</a>
+        <a href='/' className='nav-logo'>Site Logo</a>
         <ul>
+            <li>
+                <a href='#'>Home</a>
+            </li>
             <li>
                 <a href='#about'>About</a>
             </li>
             <li>
+                <a href='#contact'>Contact</a>
+            </li>
+            <li>
+                <a href='#faq'>FAQ</a>
+            </li>
+            <li>
                 <a href='#sponsers'>Sponsers</a>
+            </li>
+            <li>
+                <a href='#team'>Team</a>
             </li>
         </ul>
     </nav>
