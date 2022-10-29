d153de89f32482626e332f4cb067338669351b21
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-10-29 18:57:59 -0400

    add gradient and style navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,8 @@
 
 const Hero = () => {
     return (
-        <div id='hero'>
-            <p>Interested in CS and tech? Want to prizes while developing your coding skills?</p>
-            <p><b>Join us @ [DATE AND TIME]</b></p>
-            <button>Register Now</button>
-            <button>Cool Box</button>
-            <p class='text-center text-3xl'>as;dkfaodk</p>
+        <div class='section flex mt-4'>
+            aaa
         </div>
     );
 }
