46b79919391cb6f80b11b081a1f563793efa2c8d
Harryyoshi <75763081+HarryWang05@users.noreply.github.com>
2022-10-29 20:44:14 -0400

    Add FAQ component and swiper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
+import FAQ from './components/FAQ/FAQ.jsx';
 
 function App() {
   return (
     <div id='main'>
       <Navbar/>
       <Hero/>
+      <FAQ/>
     </div>
   );
 }
