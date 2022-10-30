57f91ee75b9e157840c9aa6e1e9c551879458664
Harry Wang <75763081+HarryWang05@users.noreply.github.com>
2022-10-30 10:24:42 -0400

    Add FAQ component and swiper (#2)

---
+++ src/components/FAQ/FAQ.jsx
@@ -0,0 +1,15 @@
+import './FAQ.scss'
+
+const Hero = () => {
+    return (
+        <div id = 'faq' class='section items-center'>
+            <h1 id = 'title' class='text-8xl mt-8 font-bold text-center'>
+                <span class='gradient'>F</span>REQUENTY <span class='gradient'>A</span>SKED
+                <br/>
+                <span class='gradient'>Q</span>UESTIONS
+            </h1>
+        </div>
+    );
+}
+
+export default Hero;
