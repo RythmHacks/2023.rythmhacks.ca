8e5a6be3ab1a820a132ff6655c8935826381d8ac
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2022-11-22 08:50:17 -0500

    SEO (#3)
    * import yellow line thingies
    
    * add some stuff
    
    * styles redo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,9 @@
 import './Hero.scss'
 
-
 const Hero = () => {
     return (
-        <div id = 'hero' class='section items-center'>
-            
-            <div id = 'cycle-wrapper' class="font-extralight">
-				<h3 class="rw-sentence ml-20">
-					<div class="rw-words rw-words-1 z-0">
-						<span>Win cool prizes at</span>
-						<span>Meet amazing people at</span>
-						<span>Enjoy free food at</span>
-						<span>Create a project at</span>
-						<span>Make lifetime memories at</span>
-						<span>Get inspired at</span>
-					</div>
-				</h3>
-			</div>
-
-            <h1 id = 'title' class='font-bold'>
-                RYTHM
-                <h1 class='gradient inline'>HɅCKS</h1>
-            </h1>
-            <h3 class='ml-8'>Interested in CS and Tech?</h3>
-        </div>
+		<div id = 'hero' class='section'>
+		</div>
     );
 }
 
