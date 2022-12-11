51c792facfe1c064553601b40f34cbdad33c759e
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-12-11 17:30:59 -0500

    respect prefers-reduced-motion and make particles more performant

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,10 +1,42 @@
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim"; 
 import { useCallback, useMemo } from "react";
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 const ParticlesComponent = (props) => {
-  const options = useMemo(() => {
 
+  let width = useWindowDimensions().width;
+  let animate = (width < 768) ? false : true;
+
+  const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches
+  if (prefersReducedMotion) { 
+    animate = false;
+  }
+
+  const options = useMemo(() => {
     return {
       background: {
         color: "#efefef", // this sets a background color for the canvas
@@ -31,7 +63,7 @@ const ParticlesComponent = (props) => {
           color: '#878787'
         },
         move: {
-          enable: true, // enabling this will make particles move in the canvas
+          enable: animate, // enabling this will make particles move in the canvas
           speed: { min: 0.1, max: 1 }, // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
         },
         opacity: {
