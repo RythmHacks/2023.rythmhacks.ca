3b7a874d02d905c7f972d38be0e9c9f90fbd9ac7
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-12-19 20:05:24 -0500

    make it a component and fix animation

---
+++ src/components/FAQ/Dropdown.jsx
@@ -0,0 +1,21 @@
+import './FAQ.scss'
+import { useRef } from 'react'
+
+const Dropdown = (props) => {
+
+    const ref = useRef(null);
+
+    return (
+        <div className='row' onClick = {
+            () => {
+                ref.current.style.maxHeight = (ref.current.style.maxHeight === '0px' || ref.current.style.maxHeight === '' || !ref.current.style.maxHeight) ? (ref.current.scrollHeight + 32) + 'px' : '0px';
+                ref.current.style.padding = (ref.current.style.maxHeight === '0px' || ref.current.style.maxHeight === '' || !ref.current.style.maxHeight) ? '0 1rem' : '1rem';
+            }
+        }>
+            <p className='q'>{props.q}</p>
+            <p ref={ref} className='a'>{props.a}</p>
+        </div>
+    )
+}
+
+export default Dropdown
--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,29 +1,23 @@
 import './FAQ.scss'
 import { useRef } from 'react'
+import Dropdown from './Dropdown.jsx'
 
 const FAQ = () => {
     const ref = useRef(null);
 
     return (
-        <div id = 'faq'>
+        <div id = 'faq' className='pt-[4rem]'>
             <h3 className='text-center mb-4'>Frequently Asked Questions</h3>
 
             <div className='faqwrapper'>
                 <div className='column'>
-                    <div className='row' onClick = {
-                            () => {
-                                console.log(typeof ref.current.scrollHeight)
-                                ref.current.style.maxHeight = (ref.current.style.maxHeight === '0px') ? (ref.current.scrollHeight + 32) + 'px' : 0;
-                                ref.current.style.padding = (ref.current.style.maxHeight === '0px') ? '0 1rem' : '1rem';
-                            }
-                        }>
-                        <p className='q'>What's a hackathon?</p>
-                        <p ref={ref} className='a'>idk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lolidk lol</p>
-                    </div>
+                    <Dropdown q='What is the purpose of this website?' a='This website is a platform for students to share their experiences and advice with other students.'/>
                 </div>
 
                 <div className='column'> 
-                    <p className='row'>somethign</p>
+                    <div className='row'>
+                        <p className='q'>somethign</p>
+                    </div>
                 </div>
             </div>
         </div>
