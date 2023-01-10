ea135b976af10d9f69d8aa1d7101036ac51f76d0
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2023-01-10 16:07:46 +0000

    sync: sync lol

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import './Footer.scss'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logosmall.png'
 import Socials from '../Socials/Socials.jsx'
 
 const Footer = () => {
@@ -7,15 +7,16 @@ const Footer = () => {
         <div id='footer'>
             
             <div className='column'>
-                <div className='logowrapper'>
-                    <img loading='lazy' alt='logo'  src={logo}></img>
+                <div className='logowrapper mb-4'>
+                    <img loading='lazy' alt='logo' src={logo} className='rounded-full'></img>
                     <h3>RythmHacks</h3>
                 </div>
+                <p className='mb-4'>Copyright © 2022 RythmHacks</p>
                 <Socials />
             </div>
 
             <div className='column'>
-                e
+                <h2>Navigation</h2>
             </div>
             
             <div className='column'>
