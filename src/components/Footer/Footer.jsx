3c05a023cb669c40c0ac4949d29753c9d183296a
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2023-01-10 15:00:54 +0000

    refactor: font size

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,17 @@ import logo from '../../assets/logo.png'
 const Footer = () => {
     return (
         <div id='footer'>
-            <div className='logowrapper'>
-                <img loading='lazy' alt='logo'  src={logo}></img>
-                <h3>RythmHacks</h3>
+            <div className='column'>
+                <div className='logowrapper'>
+                    <img loading='lazy' alt='logo'  src={logo}></img>
+                    <h3>RythmHacks</h3>
+                </div>
+            </div>
+            <div className='column'>
+                e
+            </div>
+            <div className='column'>
+                a
             </div>
         </div>
     )
