99f737ce7e2cdec3a69ca3c0ab725de5da1b7750
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-02-03 14:55:57 -0500

    feat: add medium

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
             <div className='sectionwrapper'>
                 <p >
                     Hackathons are a great way to meet other people, gain experience, or just get free merch. We want to make RythmHacks as accessible as possible so that everyone has a chance to spark their interest in tech. Feel free to contact us about any request, big or small, that you may have, or questions you have regarding the event.<br/>
+                    <br/>Want to get real insight on the organization process? Check out our <a href='https://medium.com/@rythmhacks' target='_blank' rel='noreferrer'>Medium blog</a>, which features posts written by our organizers that give you firsthand insight into the event planning process.<br/> 
                     <br/>Have questions? Need guidance about the application process? Want to sponsor or support us? Email us <a href='mailto:rythmhacks@gmail.com'>here!</a>
                 </p>
                 <img loading='lazy' src={Contactimg} alt='contact' className='w-[30%] p-0'></img>
