3efd9531176a20e1e7a983013c319d2d88174c1e
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-02-12 12:28:53 -0500

    feat: create team component

---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,13 @@
+import './Team.scss'
+
+const Team = () => {
+    
+    return (
+        <div id='team'>
+            <h3 className='text-center'>Our Team</h3>
+            <p className='text-center'>The people working to make RythmHacks a reality</p>
+        </div>
+    )
+}
+
+export default Team
