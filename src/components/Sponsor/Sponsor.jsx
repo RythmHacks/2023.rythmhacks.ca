2cfee13fc124da0920c2f2263250214ef0ddeda7
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-01 19:46:35 -0400

    add a section about startups

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -8,7 +8,9 @@ import '../ScrollAnimation/ScrollAnimation.scss'
 const Sponsor = () => {
 
   const ref1 = useRef(null);
+  const ref2 = useRef(null);
   const isInViewport1 = useIsInViewport(ref1);
+  const isInViewport2 = useIsInViewport(ref2);
 
   return (
     <div id='sponsors' className='section'>
@@ -23,13 +25,25 @@ const Sponsor = () => {
             <br/><br/>
             Without sponsors, we wouldn't be able to purchase food and prizes, host workshops, or make RythmHacks as awesome as it is. Many of our hackers will use your software or product in their projects. With some of the most creative young minds in one place, you'll have a chance to recruit the next generation of tech talent. Sponsor us today and inspire high school students to experience the magic of tech.
             <br/><br/>
+            <h4 className='mb-4 mt-4'>Supporting the <span className='gradient purple'>start-up ecosystem</span>.</h4>
+            One of our main goals is to provide support and
+            mentorship for the start-ups and entrepreneurs of the
+            future. Over the weekend, there will be special workshops,
+            chats, and resources that specifically target those wishing
+            to continue their project further. Additionally, we're giving
+            special benefits to companies that are still in the start-up
+            stage.
+            <br/><br/>
             Interested in sponsoring? Contact us at <a href='mailto:business@rythmhacks.ca'>business@rythmhacks.ca</a>. 
             <br/><br/>
             <button onClick={() => window.location = "mailto:business@rythmhacks.ca"} className='mr-4'>Contact us</button>
-            <button onClick={() => window.location.href = '/documents/prospectus.pdf'}>Sponsorship Prospectus</button>
+            <button onClick={() => window.open('https://www.rythmhacks.ca/documents/prospectus.pdf')}>Sponsorship Prospectus</button>
           </p>
           <img loading='lazy' src={SponsorImage} alt='sponsorimg'></img>
         </div>
+      </div>
+
+      <div ref={ref2} className={`${isInViewport2 ? 'hide show' : "hide"} mt-4`}>
       </div>
     </div>
   )
